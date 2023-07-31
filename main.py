987056d1283f2765465d4c750344f4ddb668b0c9
yash <yash7raut@gmail.com>
2023-07-31 23:16:27 +0530

    added the _pycache_ to the gitignore and wrote the function for /courses endpoint

--- main.py
+++ main.py
@@ -8,4 +8,40 @@ app = FastAPI()
 client = MongoClient("mongodb://localhost:27017/")
 db = client["courses"]
 
-@app.get("/courses")
+@app.get('/courses')
+def get_courses(sort_by: str = 'date', domain: str = None):
+    # set the rating.total and rating.count to all the courses based on the sum of the chapters rating
+    for course in db.courses.find():
+        total = 0
+        count = 0
+        for chapter in course['chapters']:
+            with contextlib.suppress(KeyError):
+                total += chapter['rating']['total']
+                count += chapter['rating']['count']
+        db.courses.update_one({'_id': course['_id']}, {'$set': {'rating': {'total': total, 'count': count}}})
+
+
+    # sort_by == 'date' [DESCENDING]
+    if sort_by == 'date':
+        sort_field = 'date'
+        sort_order = -1
+
+    # sort_by == 'rating' [DESCENDING]
+    elif sort_by == 'rating':
+        sort_field = 'rating.total'
+        sort_order = -1
+
+    # sort_by == 'alphabetical' [ASCENDING]
+    else:  
+        sort_field = 'name'
+        sort_order = 1
+        sort_order = 1
+
+    query = {}
+    if domain:
+        query['domain'] = domain
+
+
+    courses = db.courses.find(query, {'name': 1, 'date': 1, 'description': 1, 'domain':1,'rating':1,'_id': 0}).sort(sort_field, sort_order)
+    return list(courses)
+
