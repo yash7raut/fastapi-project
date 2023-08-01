daab1592c76c6203f89ea2263a035245faf6eb72
yash <yash7raut@gmail.com>
2023-08-01 10:58:15 +0530

    test the get courses endpoint

--- test_app.py
+++ test_app.py
@@ -7,3 +7,53 @@ from main import app
 client = TestClient(app)
 mongo_client  = MongoClient('mongodb://localhost:27017/')
 db = mongo_client['courses']
+
+def test_get_courses_no_params():
+    response = client.get("/courses")
+    assert response.status_code == 200
+
+def test_get_courses_sort_by_alphabetical():
+    response = client.get("/courses?sort_by=alphabetical")
+    assert response.status_code == 200
+    courses = response.json()
+    assert len(courses) > 0
+    assert sorted(courses, key=lambda x: x['name']) == courses
+     
+
+def test_get_courses_sort_by_date():
+    response = client.get("/courses?sort_by=date")
+    assert response.status_code == 200
+    courses = response.json()
+    assert len(courses) > 0
+    assert sorted(courses, key=lambda x: x['date'], reverse=True) == courses
+
+def test_get_courses_sort_by_rating():
+    response = client.get("/courses?sort_by=rating")
+    assert response.status_code == 200
+    courses = response.json()
+    assert len(courses) > 0
+    assert sorted(courses, key=lambda x: x['rating']['total'], reverse=True) == courses
+
+def test_get_courses_filter_by_domain():
+    response = client.get("/courses?domain=mathematics")
+    assert response.status_code == 200
+    courses = response.json()
+    assert len(courses) > 0
+    assert all([c['domain'][0] == 'mathematics' for c in courses])
+    
+def test_get_courses_filter_by_domain_and_sort_by_alphabetical():
+    response = client.get("/courses?domain=mathematics&sort_by=alphabetical")
+    assert response.status_code == 200
+    courses = response.json()
+    assert len(courses) > 0
+    assert all([c['domain'][0] == 'mathematics' for c in courses])
+    assert sorted(courses, key=lambda x: x['name']) == courses
+    
+def test_get_courses_filter_by_domain_and_sort_by_date():
+    response = client.get("/courses?domain=mathematics&sort_by=date")
+    assert response.status_code == 200
+    courses = response.json()
+    assert len(courses) > 0
+    assert all([c['domain'][0] == 'mathematics' for c in courses])
+    assert sorted(courses, key=lambda x: x['date'], reverse=True) == courses
+    
